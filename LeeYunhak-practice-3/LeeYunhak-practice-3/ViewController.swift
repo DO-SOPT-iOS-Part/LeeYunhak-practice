92acaf240d225468d608c967b0173eadab4cdd84
YunHak Lee <lyh990408@gmail.com>
2023-10-28 14:22:35 +0900

    [SETTING] 3주차 세미나 기초 세팅 - Snapkit, then 패키지 의존성 추가

---
+++ LeeYunhak-practice-3/LeeYunhak-practice-3/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LeeYunhak-practice-3
+//
+//  Created by 이윤학 on 10/28/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        view.backgroundColor = .blue
+    }
+
+
+}
+
