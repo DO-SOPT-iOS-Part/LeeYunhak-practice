7aba9e432f1af93cc619345b7bce84162b8dbeff
YunHak Lee <lyh990408@gmail.com>
2023-12-05 15:18:10 +0900

    7주차 실습

---
+++ LeeYunhak-practice-7/LeeYunhak-practice-7/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LeeYunhak-practice-7
+//
+//  Created by 이윤학 on 12/2/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
